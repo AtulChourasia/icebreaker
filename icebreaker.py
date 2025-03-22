9e4fe57764be2a874e4a252c4f74a594cdf939e1
Atul Chourasia <a8406@axtria.com>
2025-03-22 14:01:39 +0530

    initial setup

---
+++ icebreaker.py
@@ -0,0 +1,12 @@
+import os
+import langchain
+import langchain_community
+import langchain_openai
+import langchainhub
+
+
+
+if __name__ == "__main__":
+    print("Hello, World!")
+    print(os.environ['OPENAI_API_KEY'])
+    
